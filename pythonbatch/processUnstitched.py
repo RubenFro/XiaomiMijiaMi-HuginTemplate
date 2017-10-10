84170ba6ab7d48a9be1a80b11bcd17ef12e8e914
pxlartificer <ruben@pxlartificer.com>
2017-10-10 09:42:16 +0900

    Add files via upload

---
+++ pythonbatch/processUnstitched.py
@@ -0,0 +1,29 @@
+#!/usr/local/bin/python
+
+# Set default logging handler to avoid "No handler found" warnings.
+import glob
+
+import string 
+
+# Here add the path to the default template you want to use...
+templateString = open('templates/template-XiaomiMijiaMi.pto', 'r').read()
+
+# Path of the unstitched images - Copy your JPG files in the "files" folder
+files = glob.glob("files/*.JPG")
+
+for entry in files:
+	filenameImage=entry.replace('files/', '')
+	filenamePTOOutput=entry.replace('.JPG', '.pto')
+
+	stringToWrite=templateString
+	stringToWrite = string.replace(stringToWrite,'image.jpg',filenameImage)
+	
+	text_file = open(filenamePTOOutput, "w")
+	text_file.write(stringToWrite)
+	text_file.close()
+
+
+
+
+	
+	
